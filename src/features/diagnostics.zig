161b52c6fb0a01fa4fa5ce8d230a97f7b3363487
David Strachan <davidstrachan92@gmail.com>
2024-01-13 23:00:44 +0000

    Group by block

--- src/features/diagnostics.zig
+++ src/features/diagnostics.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const lsp = @import("lsp");
 const types = lsp.types;
 
+const kdb = @import("../kdb.zig");
+const Token = kdb.Token;
 const Server = @import("../Server.zig");
 const DocumentStore = @import("../DocumentStore.zig");
 const offsets = @import("../offsets.zig");
@@ -12,19 +14,46 @@ pub fn generateDiagnostics(server: *Server, arena: std.mem.Allocator, handle: *D
     const tree = handle.tree;
 
     var diagnostics = std.ArrayListUnmanaged(types.Diagnostic){};
-    try diagnostics.ensureUnusedCapacity(arena, tree.tokens.len);
-    for (tree.tokens.items(.tag), 0..) |tag, i| {
-        var buffer = std.ArrayListUnmanaged(u8){};
-        try buffer.writer(arena).writeAll(tag.symbol());
+    var start: ?usize = null;
+    const start_index: usize = if (tree.tokens.items(.tag)[0] == .comment) 1 else 0;
+    for (tree.tokens.items(.tag)[start_index..], tree.tokens.items(.loc)[start_index..], tree.tokens.items(.eob)[start_index..]) |tag, loc, eob| {
+        if (tag == .eof) break;
+        if (start == null) {
+            start = loc.start;
+        }
+        if (eob) {
+            var buffer = std.ArrayListUnmanaged(u8){};
+            try buffer.writer(arena).writeAll("TEST");
 
-        diagnostics.appendAssumeCapacity(.{
-            .range = offsets.tokenToRange(tree, @intCast(i), server.position_encoding),
-            .severity = .Information,
-            .code = .{ .string = @tagName(tag) },
-            .source = "kdblint",
-            .message = try buffer.toOwnedSlice(arena),
-        });
+            const range_start = offsets.indexToPosition(tree.source, start.?, server.position_encoding);
+            const range_end = offsets.indexToPosition(tree.source, loc.end, server.position_encoding);
+            try diagnostics.append(arena, .{
+                .range = .{
+                    .start = range_start,
+                    .end = range_end,
+                },
+                .severity = .Information,
+                .code = .{ .string = @tagName(tag) },
+                .source = "kdblint",
+                .message = try buffer.toOwnedSlice(arena),
+            });
+            start = null;
+        }
     }
+
+    // try diagnostics.ensureUnusedCapacity(arena, tree.tokens.len);
+    // for (tree.tokens.items(.tag), 0..) |tag, i| {
+    //     var buffer = std.ArrayListUnmanaged(u8){};
+    //     try buffer.writer(arena).writeAll(tag.symbol());
+
+    //     diagnostics.appendAssumeCapacity(.{
+    //         .range = offsets.tokenToRange(tree, @intCast(i), server.position_encoding),
+    //         .severity = .Information,
+    //         .code = .{ .string = @tagName(tag) },
+    //         .source = "kdblint",
+    //         .message = try buffer.toOwnedSlice(arena),
+    //     });
+    // }
 
     return .{
         .uri = handle.uri,
