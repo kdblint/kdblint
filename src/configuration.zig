579fc82601294092b669a972e59daa8957d9131d
David Strachan <davidstrachan92@gmail.com>
2024-05-13 19:36:46 +0100

    Cleanup

--- src/configuration.zig
+++ src/configuration.zig
@@ -142,50 +142,6 @@ pub fn loadFromFile(allocator: std.mem.Allocator, file_path: []const u8) error{O
     } };
 }
 
-pub const Env = struct {
-    zig_exe: []const u8,
-    lib_dir: ?[]const u8,
-    std_dir: []const u8,
-    global_cache_dir: []const u8,
-    version: []const u8,
-    target: ?[]const u8 = null,
-};
-
-pub fn getZigEnv(allocator: std.mem.Allocator, zig_exe_path: []const u8) ?std.json.Parsed(Env) {
-    const zig_env_result = std.process.Child.run(.{
-        .allocator = allocator,
-        .argv = &[_][]const u8{ zig_exe_path, "env" },
-    }) catch {
-        logger.err("Failed to execute zig env", .{});
-        return null;
-    };
-
-    defer {
-        allocator.free(zig_env_result.stdout);
-        allocator.free(zig_env_result.stderr);
-    }
-
-    switch (zig_env_result.term) {
-        .Exited => |code| {
-            if (code != 0) {
-                logger.err("zig env failed with error_code: {}", .{code});
-                return null;
-            }
-        },
-        else => logger.err("zig env invocation failed", .{}),
-    }
-
-    return std.json.parseFromSlice(
-        Env,
-        allocator,
-        zig_env_result.stdout,
-        .{ .ignore_unknown_fields = true, .allocate = .alloc_always },
-    ) catch {
-        logger.err("Failed to parse zig env JSON result", .{});
-        return null;
-    };
-}
-
 /// the same struct as Config but every field is optional
 pub const Configuration = getConfigurationType();
 
@@ -205,51 +161,6 @@ fn getConfigurationType() type {
     config_info.Struct.fields = fields[0..];
     config_info.Struct.decls = &.{};
     return @Type(config_info);
-}
-
-pub fn findZig(allocator: std.mem.Allocator) error{OutOfMemory}!?[]const u8 {
-    const env_path = std.process.getEnvVarOwned(allocator, "PATH") catch |err| switch (err) {
-        error.EnvironmentVariableNotFound => return null,
-        error.OutOfMemory => |e| return e,
-        error.InvalidWtf8 => |e| {
-            logger.err("failed to load 'PATH' enviorment variable: {}", .{e});
-            return null;
-        },
-    };
-    defer allocator.free(env_path);
-
-    const zig_exe = "zig" ++ comptime builtin.target.exeFileExt();
-
-    var it = std.mem.tokenizeScalar(u8, env_path, std.fs.path.delimiter);
-    while (it.next()) |path| {
-        var full_path = try std.fs.path.join(allocator, &[_][]const u8{ path, zig_exe });
-        defer allocator.free(full_path);
-
-        if (!std.fs.path.isAbsolute(full_path)) {
-            logger.warn("ignoring entry in PATH '{s}' because it is not an absolute file path", .{full_path});
-            continue;
-        }
-
-        const file = std.fs.openFileAbsolute(full_path, .{}) catch |err| switch (err) {
-            error.FileNotFound => continue,
-            else => |e| {
-                logger.warn("failed to open entry in PATH '{s}': {}", .{ full_path, e });
-                continue;
-            },
-        };
-        defer file.close();
-
-        stat_failed: {
-            const stat = file.stat() catch break :stat_failed;
-            if (stat.kind == .directory) {
-                logger.warn("ignoring entry in PATH '{s}' because it is a directory", .{full_path});
-            }
-        }
-
-        defer full_path = "";
-        return full_path;
-    }
-    return null;
 }
 
 test {
