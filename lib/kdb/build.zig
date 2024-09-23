c8d055b4d08a30cfcc8602ea4f45ef4f491eed97
David Strachan <david@kdblint.com>
2024-09-23 20:01:56 +0100

    KLS-141 Setup debugging for fmt

--- lib/kdb/build.zig
+++ lib/kdb/build.zig
@@ -4,11 +4,33 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("kdb", .{
+    const kdb_mod = b.addModule("kdb", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const exe = b.addExecutable(.{
+        .name = "fmt",
+        .root_source_file = b.path("src/fmt.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("kdb", kdb_mod);
+
+    b.installArtifact(exe);
+
+    const check_step = b.step("check", "Compile project without build artifacts");
+    check_step.dependOn(&exe.step);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
