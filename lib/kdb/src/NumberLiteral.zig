385e927e5e545e8eeb6cde885ca457ba3a99b18a
David Strachan <davidstrachan92@gmail.com>
2025-01-01 13:00:46 +0000

    KLS-214 Add char

--- lib/kdb/src/NumberLiteral.zig
+++ lib/kdb/src/NumberLiteral.zig
@@ -162,7 +162,7 @@ pub fn parse(bytes: []const u8, type_hint: TypeHint, allow_suffix: bool) Result 
         .long => return parseLong(bytes, allow_suffix),
         .real => return .{ .failure = .nyi },
         .float => return .{ .failure = .nyi },
-        .char => return .{ .failure = .nyi },
+        .char => return parseChar(bytes, allow_suffix),
         .timestamp => return .{ .failure = .nyi },
         .month => return .{ .failure = .nyi },
         .date => return .{ .failure = .nyi },
@@ -627,6 +627,24 @@ fn parseLong(bytes: []const u8, allow_suffix: bool) Result {
     return .{ .long = x };
 }
 
+fn parseChar(bytes: []const u8, allow_suffix: bool) Result {
+    return switch (bytes.len) {
+        0 => unreachable,
+        1 => switch (bytes[0]) {
+            '0'...'9' => .{ .char = bytes[0] },
+            else => .{ .char = null_char },
+        },
+        2 => switch (bytes[0]) {
+            '0'...'9' => if (allow_suffix and bytes[1] == 'c')
+                .{ .char = bytes[0] }
+            else
+                .{ .char = null_char },
+            else => .{ .char = null_char },
+        },
+        else => .{ .char = null_char },
+    };
+}
+
 fn testParse(bytes: []const u8, type_hint: TypeHint, allow_suffix: bool, expected: Result) !void {
     const result = parse(bytes, type_hint, allow_suffix);
     try std.testing.expectEqual(expected, result);
@@ -869,4 +887,36 @@ test "parse number literal - long" {
     try testParse("0j", .long, true, .{ .long = 0 });
     try testParse("1j", .long, true, .{ .long = 1 });
     try testParse("2j", .long, true, .{ .long = 2 });
+}
+
+test "parse number literal - char" {
+    try testParse("0n", .char, false, .{ .char = null_char });
+    try testParse("0N", .char, false, .{ .char = null_char });
+    try testParse("0w", .char, false, .{ .char = null_char });
+    try testParse("0W", .char, false, .{ .char = null_char });
+    try testParse("0", .char, false, .{ .char = '0' });
+    try testParse("1", .char, false, .{ .char = '1' });
+    try testParse("2", .char, false, .{ .char = '2' });
+    try testParse("0n", .char, true, .{ .char = null_char });
+    try testParse("0N", .char, true, .{ .char = null_char });
+    try testParse("0w", .char, true, .{ .char = null_char });
+    try testParse("0W", .char, true, .{ .char = null_char });
+    try testParse("0", .char, true, .{ .char = '0' });
+    try testParse("1", .char, true, .{ .char = '1' });
+    try testParse("2", .char, true, .{ .char = '2' });
+
+    try testParse("0nc", .char, false, .{ .char = null_char });
+    try testParse("0Nc", .char, false, .{ .char = null_char });
+    try testParse("0wc", .char, false, .{ .char = null_char });
+    try testParse("0Wc", .char, false, .{ .char = null_char });
+    try testParse("0c", .char, false, .{ .char = null_char });
+    try testParse("1c", .char, false, .{ .char = null_char });
+    try testParse("2c", .char, false, .{ .char = null_char });
+    try testParse("0nc", .char, true, .{ .char = null_char });
+    try testParse("0Nc", .char, true, .{ .char = null_char });
+    try testParse("0wc", .char, true, .{ .char = null_char });
+    try testParse("0Wc", .char, true, .{ .char = null_char });
+    try testParse("0c", .char, true, .{ .char = '0' });
+    try testParse("1c", .char, true, .{ .char = '1' });
+    try testParse("2c", .char, true, .{ .char = '2' });
 }
